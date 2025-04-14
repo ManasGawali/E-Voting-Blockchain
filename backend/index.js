5d66e91e68e476300de0763447f89e2a8af68b74
ManasGawali <manasmgawali@gmail.com>
2025-04-14 12:52:05 +0530

    My community updated to give community names, CORS added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express=require("express");
-
+const cors=require("cors");
 const app=express();
 const login=require("../backend/routes/users/users_login");
 const CommunityCreation=require("../backend/routes/admin/create_community");
@@ -8,6 +8,12 @@ const myCommunity=require("./routes/users/my_community");
 const getSchema=require("./routes/admin/dynamic_schema_fetch");
 
 // console.log(admin.name);
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Include OPTIONS
+    allowedHeaders: ["Content-Type", "token"], // Allow custom headers like 'token'
+    optionsSuccessStatus: 200,
+  }));
 app.use(express.json());
 app.use("/user", login);
 app.use("/admin/createCommunity",CommunityCreation);
